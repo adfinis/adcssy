dff0042406b3d3212e55f23a7b9bb934f27cc667
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-09-04 17:00:00 +0200

    Add unminified build and external sourcemaps

--- Brocfile.js
+++ Brocfile.js
@@ -2,28 +2,37 @@
 
 'use strict'
 
-let cssnext = broc('cssnext-single')
-let funnel  = broc('funnel')
-let merge   = broc('merge-trees')
-let live    = broc('inject-livereload')
-let env     = broc('env').getEnv()
+let merge      = require('lodash.merge')
+let cssnext    = broc('cssnext-single')
+let funnel     = broc('funnel')
+let mergeTrees = broc('merge-trees')
+let live       = broc('inject-livereload')
+let env        = broc('env').getEnv()
 
-let ionicons = funnel('node_modules/font-awesome/fonts', {
-  destDir: 'fonts'
-})
-
-let assets = merge([ funnel('assets'), ionicons ])
-
-let css = cssnext('css', 'adcssy.css', 'css/adcssy.css', {
-  compress:  env === 'production',
-  sourcemap: env !== 'production',
+let cssOptions = {
   url:       false,
   browsers:  '> 1%, last 2 versions, Firefox ESR',
   features:  { rem: false },
   plugins:   [ plugin('responsive-type') ]
+}
+
+let icons = funnel('node_modules/font-awesome/fonts', {
+  destDir: 'fonts'
 })
 
-let trees = [ css, assets ]
+let assets = mergeTrees([ funnel('assets'), icons ])
+
+let css = cssnext('css', 'adcssy.css', 'css/adcssy.css', merge({
+  map:      { inline: false, annotation: 'adcssy.css.map' },
+  compress: false
+}, cssOptions))
+
+let minified = cssnext('css', 'adcssy.css', 'css/adcssy.min.css', merge({
+  map:      { inline: false, annotation: 'adcssy.min.css.map' },
+  compress: true
+}, cssOptions))
+
+let trees = [ css, minified, assets ]
 
 if (env === 'development') {
   let html = live('test', { destDir: '/' })
@@ -31,7 +40,7 @@ if (env === 'development') {
   trees.push(html)
 }
 
-module.exports = merge(trees)
+module.exports = mergeTrees(trees)
 
 function broc(m) {
   return require('broccoli-' + m)
