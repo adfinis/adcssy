707451badb95b6fb32657bd751e50d3ad42ad1f7
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-10-21 17:59:16 +0200

    Switch to postcss

--- Brocfile.js
+++ Brocfile.js
@@ -2,19 +2,28 @@
 
 'use strict'
 
-let merge      = require('lodash.merge')
-let cssnext    = broc('cssnext-single')
+let cssnano    = require('cssnano')
+let postcss    = broc('postcss')
 let funnel     = broc('funnel')
 let mergeTrees = broc('merge-trees')
 let live       = broc('inject-livereload')
 let env        = broc('env').getEnv()
 
-let cssOptions = {
-  url:       false,
-  browsers:  '> 2%, last 2 versions, Firefox 38',
-  features:  { rem: false },
-  plugins:   [ plugin('responsive-type') ]
-}
+let plugins = [
+  {
+    module: plugin('import')
+  },
+  {
+    module: plugin('cssnext'),
+    options: {
+      browsers: '> 2%, last 2 versions, Firefox 38',
+      features: { rem: false }
+    }
+  },
+  {
+    module: plugin('responsive-type')
+  }
+]
 
 let icons = funnel('node_modules/font-awesome/fonts', {
   destDir: 'fonts'
@@ -22,15 +31,17 @@ let icons = funnel('node_modules/font-awesome/fonts', {
 
 let assets = mergeTrees([ funnel('assets'), icons ])
 
-let css = cssnext('css', 'adcssy.css', 'css/adcssy.css', merge({
-  map:      { inline: false, annotation: 'adcssy.css.map' },
-  compress: false
-}, cssOptions))
+let css = postcss(['css'], 'adcssy.css', 'css/adcssy.css', plugins, {
+  inline:     false,
+  annotation: 'adcssy.css.map'
+})
 
-let minified = cssnext('css', 'adcssy.css', 'css/adcssy.min.css', merge({
-  map:      { inline: false, annotation: 'adcssy.min.css.map' },
-  compress: true
-}, cssOptions))
+plugins = plugins.concat([{ module: cssnano }])
+
+let minified = postcss(['css'], 'adcssy.css', 'css/adcssy.min.css', plugins, {
+  inline:     false,
+  annotation: 'adcssy.min.css.map'
+})
 
 let trees = [ css, minified, assets ]
 
