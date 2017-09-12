4b605acb831b700fe3f1af5683d40851ea40ce5e
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-09-12 14:09:27 +0200

    Update dependencies and replace livereload with browsersync

--- Brocfile.js
+++ Brocfile.js
@@ -2,61 +2,54 @@
 
 'use strict'
 
-let cssnano    = require('cssnano')
-let postcss    = broc('postcss')
-let funnel     = broc('funnel')
-let mergeTrees = broc('merge-trees')
-let live       = broc('inject-livereload')
-let env        = broc('env').getEnv()
+const postcss = require('broccoli-postcss-single')
+const Funnel = require('broccoli-funnel')
+const BroccoliMergeTrees = require('broccoli-merge-trees')
+const BrowserSync = require('broccoli-browser-sync')
+const env = require('broccoli-env').getEnv()
 
 let plugins = [
+  { module: require('postcss-import') },
   {
-    module: plugin('import')
-  },
-  {
-    module: plugin('cssnext'),
+    module: require('postcss-cssnext'),
     options: {
       browsers: '> 2%, last 2 versions, Firefox ESR',
       features: { rem: false }
     }
   },
-  {
-    module: plugin('responsive-type')
-  }
+  { module: require('postcss-responsive-type') }
 ]
 
-let icons = funnel('node_modules/font-awesome/fonts', {
+let icons = new Funnel('node_modules/font-awesome/fonts', {
   destDir: 'fonts'
 })
 
-let assets = mergeTrees([ funnel('assets'), icons ])
+let assets = new BroccoliMergeTrees(['assets', icons])
 
-let css = postcss(['css'], 'adcssy.css', 'css/adcssy.css', plugins, {
-  inline:     false,
-  annotation: 'adcssy.css.map'
+let css = postcss(['css'], 'adcssy.css', 'css/adcssy.css', {
+  plugins,
+  map: {
+    inline: false,
+    annotation: 'adcssy.css.map'
+  }
 })
 
-plugins = plugins.concat([{ module: cssnano }])
-
-let minified = postcss(['css'], 'adcssy.css', 'css/adcssy.min.css', plugins, {
-  inline:     false,
-  annotation: 'adcssy.min.css.map'
+let minified = postcss(['css'], 'adcssy.css', 'css/adcssy.min.css', {
+  plugins: [...plugins, { module: require('cssnano') }],
+  map: {
+    inline: false,
+    annotation: 'adcssy.min.css.map'
+  }
 })
 
-let trees = [ css, minified, assets ]
+let trees = [css, minified, assets]
 
 if (env === 'development') {
-  let html = live('test', { destDir: '/' })
+  let html = new Funnel('test')
 
   trees.push(html)
-}
 
-module.exports = mergeTrees(trees)
-
-function broc(m) {
-  return require('broccoli-' + m)
-}
-
-function plugin(p) {
-  return require('postcss-' + p)
+  module.exports = new BroccoliMergeTrees([...trees, new BrowserSync(trees)])
+} else {
+  module.exports = new BroccoliMergeTrees(trees)
 }
